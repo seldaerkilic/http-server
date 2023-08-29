504211e6815e962464cb4cc0674dd8f9385f97ea
Selda Erkilic <seldaerkilic@gmail.com>
2023-08-29 16:41:02 -0400

    added more data to JSON, and parameterized URL with endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const PORT = 3000;
 
+
+
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, {
 //         'Content-Type': 'application/json',
@@ -12,21 +14,41 @@ const PORT = 3000;
 //     }));
 // });
 
+const friends = [
+    {
+        id: 0,
+        name: 'Nicola Tesla',
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newton',
+    },
+    {
+        id: 2,
+        name: 'Albert Einstein',
+    }
+]
+
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
+    const items = req.url.split('/');
+    // /friends/2 => ['', 'friends', '2']
+
+    if (items[1] === 'friends') {
         // writeHead default is 200. No need for code below. However, if we want to write it, there's another way following it:
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Isaac Newton',
-        }));
-    } else if (req.url === '/messages') {
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
